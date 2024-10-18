ebb88474eed3842b992f6df72010e9a41e64dc13
Santiago Ochoa <santiago439s@gmail.com>
2024-10-18 15:33:41 -0500

    Base Spring Boot

---
+++ src/main/java/com/franchises/franchises_crud/FranchisesCrudApplication.java
@@ -0,0 +1,13 @@
+package com.franchises.franchises_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranchisesCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranchisesCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franchises/franchises_crud/FranchisesCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franchises.franchises_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchisesCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
