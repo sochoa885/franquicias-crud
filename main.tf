68e3ae7f96d42c5604ab423322186c0d9a1d6e32
Santiago Ochoa <santiago439s@gmail.com>
2024-10-20 03:39:41 -0500

    Configuracion de grupos de seguridad

--- main.tf
+++ main.tf
@@ -13,20 +13,16 @@ provider "aws" {
   secret_key = var.secret_key
 }
 
-resource "aws_db_instance" "franchises-crud-bd" {
-  allocated_storage    = 20
-  engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t3.micro"
-  username             = var.db_user
-  password             = var.db_password
-  parameter_group_name = "default.mysql8.0"
-  skip_final_snapshot  = true
-}
-
 resource "aws_security_group" "franchises-crud-vpc" {
   name        = "franchises-crud-vpc"
   description = "franchises-crud-vpc"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["18.206.107.24/29"]
+  }
 
   ingress {
     from_port   = 80
@@ -48,10 +44,46 @@ resource "aws_security_group" "franchises-crud-vpc" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
 
-  tags = {
-    Name = "8080 and 80"
+resource "aws_security_group" "franchises-bd-vpc" {
+  name        = "franchises-bd-vpc"
+  description = "franchises-bd-vpc"
+
+  ingress {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group_rule" "allow_ec2_to_db" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.franchises-bd-vpc.id
+  source_security_group_id = aws_security_group.franchises-crud-vpc.id
+}
+
+
+resource "aws_db_instance" "franchises-crud-bd" {
+  allocated_storage      = 20
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  username               = var.db_user
+  password               = var.db_password
+  parameter_group_name   = "default.mysql8.0"
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.franchises-bd-vpc.id]
 }
 
 resource "aws_instance" "franchises-crud" {
@@ -60,11 +92,11 @@ resource "aws_instance" "franchises-crud" {
   user_data              = <<-EOF
               #!/bin/bash
               sudo apt update -y
-              sudo apt install -y git docker.io docker-compose mysql-client-core-8.0 vim
-              mkdir -p /home/ubuntu/repository
-              git clone https://github.com/sochoa885/franquicias-crud.git /home/ubuntu/repository
-              sudo chown -R ubuntu:ubuntu /home/ubuntu/repository
-              mysql --host=${aws_db_instance.franchises-crud-bd.address} --user=${var.db_user} --${var.db_password} > /home/ubuntu/repository/franquicias-crud/init.sql
+              sudo apt install -y git mysql-client-core-8.0 docker.io docker-compose vim
+              mkdir -p /home/ubuntu/franchises-crud
+              git clone https://github.com/sochoa885/franquicias-crud.git /home/ubuntu/franchises-crud
+              sudo chown -R ubuntu:ubuntu /home/ubuntu/franchises-crud
+              mysql --host=${aws_db_instance.franchises-crud-bd.address} --user=${var.db_user} --password=${var.db_password} -e "CREATE DATABASE public;"
               EOF
   vpc_security_group_ids = [aws_security_group.franchises-crud-vpc.id]
   depends_on             = [aws_db_instance.franchises-crud-bd]
--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,5 @@
 output "db_endpoint" {
-  description = "The endpoint of the RDS instance"
-  value       = aws_db_instance.franchises-crud-bd.endpoint
+  value = aws_db_instance.franchises-crud-bd.endpoint
 }
 
 output "instance_public_ip" {
